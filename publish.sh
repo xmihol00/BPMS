8997601044186f15cc2b0a52e9e6d6174bf164d0
David Mihola <xmihol00@vutbr.cz>
2022-03-13 21:06:53 +0100

    conf

--- publish.sh
+++ publish.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+sudo mkdir -p /var/dotnet
+sudo mkdir -p /var/dotnet/sys1
+sudo mkdir -p /var/dotnet/sys2
+
 sudo dotnet publish -o /var/dotnet/sys1
-cp Config/appsettings.sys1.json /var/dotnet/sys1
+sudo cp Config/appsettings.sys1.json /var/dotnet/sys1
 
 sudo systemctl stop bpms1.service
 sudo systemctl enable bpms1.service
 sudo systemctl start bpms1.service
 
 sudo dotnet publish -o /var/dotnet/sys2
-cp Config/appsettings.sys2.json /var/dotnet/sys2
+sudo cp Config/appsettings.sys2.json /var/dotnet/sys2
 
 sudo systemctl stop bpms2.service
 sudo systemctl enable bpms2.service
