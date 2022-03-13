8f2fe8c6ae70b15b463d3fe940f9bc071fad5786
dejv.mihola <david.mihola@sperling.cz>
2022-03-13 13:12:55 +0100

    system JS

--- BPMS/wwwroot/js/System/System.js
+++ BPMS/wwwroot/js/System/System.js
@@ -5,5 +5,5 @@ function SystemUpdate(result)
 
     let buttonDiv = document.getElementById("SystemBtnsId");
     buttonDiv.children[0].remove();
-    buttonDiv.innerHTML = <button class="butn btn-d" onclick="Deactive()">Deaktivovat</button> + buttonDiv.innerHTML;
+    buttonDiv.innerHTML = `<button class="butn btn-d" onclick="Deactive()">Deaktivovat</button>` + buttonDiv.innerHTML;
 }
