1f436c95ce646c9c3e1bb6dfa97352657fda1ec4
dejv.mihola <david.mihola@sperling.cz>
2022-03-13 19:07:18 +0100

    http config

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo dotnet publish -o /var/dotnet/sys1
+cp Config/appsettings.sys1.json /var/dotnet/sys1
+
+sudo systemctl stop bpms1.service
+sudo systemctl enable bpms1.service
+sudo systemctl start bpms1.service
+
+sudo dotnet publish -o /var/dotnet/sys2
+cp Config/appsettings.sys2.json /var/dotnet/sys2
+
+sudo systemctl stop bpms2.service
+sudo systemctl enable bpms2.service
+sudo systemctl start bpms2.service
+
+sleep 3
+sudo systemctl status bpms1.service
+sudo systemctl status bpms2.service
